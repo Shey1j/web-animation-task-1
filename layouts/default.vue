68c8892e0689d9f5d87c0047abe474bdd24029af
Shey1j <seyi.juliana@gmail.com>
2022-07-23 23:22:16 +0100

    initialize splitting

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,67 +5,18 @@
 </template>
 
 <script>
+// import { gsap } from 'gsap'
+import Splitting from 'splitting'
+import 'splitting/dist/splitting.css'
+import 'splitting/dist/splitting-cells.css'
 export default {
   name: 'DefaultLayout',
-  data () {
-    return {
-      myScroll: null,
-      windowTop: 0,
-      lastScrollTop: 0
-    }
-  },
   mounted () {
-    this.$nextTick(() => {
-      this.initializeScroll()
-    })
+    this.getSplitting()
   },
   methods: {
-    initializeScroll () {
-      this.myScroll = new this.LocomotiveScroll({
-        el: document.querySelector('html'),
-        direction: 'vertical',
-        smooth: true,
-        scrollingClass: 'has-scroll-scrolling',
-        smoothClass: 'has-scroll-smooth',
-        initClass: 'has-scroll-init'
-      })
-      this.myScroll.on('scroll', (args) => {
-        this.handleNavbarUpdate(args.scroll)
-        this.handleDoorDataUpdate()
-      })
-    },
-    handleNavbarUpdate (scroll) {
-      const el = document.getElementById('nav-header')
-      this.windowTop = scroll.y
-      if (this.windowTop > this.lastScrollTop) {
-        if (this.windowTop > 0 && this.windowTop < 240) {
-          el.classList.add('white')
-        } else if (this.windowTop > 250) {
-          el.classList.add('is-scrolled')
-          setTimeout(() => {
-            el.classList.remove('white')
-          }, 1000)
-        } else {
-          el.classList.remove('is-scrolled')
-          el.classList.remove('white')
-        }
-      } else if (this.windowTop <= this.lastScrollTop) {
-        if (this.windowTop > 100) {
-          el.classList.remove('is-scrolled')
-          el.classList.add('white')
-        } else {
-          el.classList.remove('is-scrolled')
-          el.classList.remove('white')
-        }
-      }
-      this.lastScrollTop = this.windowTop
-    },
-    handleDoorDataUpdate () {
-      const element = document.getElementById('door-section')
-      const elemRect = element.getBoundingClientRect()
-      if (elemRect.top < 1) {
-        this.$store.commit('handleValueUpdate', true)
-      }
+    getSplitting () {
+      Splitting()
     }
   }
 }
