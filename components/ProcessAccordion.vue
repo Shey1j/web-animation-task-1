14cafaaa4f8eaf5528b7c7daa124a118ea2d432d
Shey1j <seyi.juliana@gmail.com>
2022-07-23 23:19:34 +0100

    create process section

---
+++ components/ProcessAccordion.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="process-accordion" :class="{active: activeId.includes(index)}" @click="handleSetActive(index)">
+    <div class="process-accordion__wrapper">
+      <div class="process-title">
+        <h3 class="process-title__title" data-splitting="lines" data-animation="text">
+          {{ title }}
+        </h3>
+        <div class="process-title__image">
+          <img :src="image" alt="">
+        </div>
+      </div>
+      <ul class="process-activities">
+        <li v-for="(activity, id) in activities" :key="id" data-splitting="lines" data-animation="text">
+          {{ activity }}
+        </li>
+      </ul>
+      <div class="process-icon">
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="31.755" height="23.402" viewBox="0 0 31.755 23.402">
+            <g id="Layer_2" data-name="Layer 2" transform="translate(-2.87 9.537)">
+              <g id="Layer_1" data-name="Layer 1" transform="translate(2.87 -9.537)">
+                <path id="Path_33196" data-name="Path 33196" d="M2.87,110v2.645l10.057,9.257h4.027l-9.974-9.18H34.626V110Z" transform="translate(-2.87 -98.505)" fill="#dc7550" />
+                <path id="Path_33195" data-name="Path 33195" d="M10.507,0,6.66.748,0,9.779l3.972-.772Z" transform="translate(3.867 0) rotate(11)" fill="#dc7550" />
+              </g>
+            </g>
+          </svg>
+        </span>
+      </div>
+    </div>
+    <span class="process-line" data-animation="line" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    index: {
+      type: Number,
+      required: true
+    },
+    activeId: {
+      type: Array,
+      required: true
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    image: {
+      type: String,
+      default: ''
+    },
+    activities: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    handleSetActive (id) {
+      this.$emit('set-active', id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.process-accordion .process-accordion__wrapper {
+    padding-top: 4rem;
+    padding-bottom: 3.9rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    cursor: pointer;
+}
+
+.process-accordion .process-accordion__wrapper .process-title {
+    flex-shrink: 1;
+    width: 100%;
+    line-height: 1;
+    max-width: 42rem;
+    margin-right: 2.6rem;
+}
+
+.process-accordion .process-accordion__wrapper .process-title__title {
+  font-family: 'Telegraf';
+  font-size: 2rem;
+}
+
+.process-accordion:hover .process-accordion__wrapper .process-title__title {
+  color: #dc7550;
+}
+
+.process-accordion .process-accordion__wrapper .process-title__image {
+  width: 12.6rem;
+  height: 12.6rem;
+  margin-top: 2.4rem;
+  border-radius: 50%;
+  display: none;
+  opacity: 0;
+  transform: scale(0);
+  transition: opacity .5s ease-in-out;
+  transition: transform .5s ease-in-out;
+}
+
+.process-accordion.active .process-accordion__wrapper .process-title__image {
+  display: block;
+  opacity: 1;
+  transform: scale(1);
+  flex-shrink: 0;
+}
+
+.process-accordion .process-accordion__wrapper .process-title__image img {
+    width: 100%;
+    height: 100%;
+    -o-object-fit: cover;
+    object-fit: cover;
+    border-radius: 50%;
+}
+
+.process-accordion .process-accordion__wrapper .process-activities {
+    height: 0;
+    opacity: 0;
+    overflow-y: hidden;
+    display: flex;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    flex-grow: 1;
+    width: 50%;
+    padding-left: 3rem;
+    min-width: 42rem;
+}
+
+.process-accordion.active .process-accordion__wrapper .process-activities {
+    height: auto;
+    opacity: 1;
+    display: flex;
+}
+
+.process-accordion .process-accordion__wrapper .process-activities li {
+    width: 49%;
+    list-style: none;
+    color: rgba(18,18,18,.8);
+    margin-bottom: 2.2rem;
+    position: relative;
+    line-height: 1.3;
+}
+
+.process-accordion .process-accordion__wrapper .process-activities li:before {
+    content: "";
+    width: 0.7rem;
+    height: 1rem;
+    background-color: #ffeba2;
+    margin-right: 1.4rem;
+    border-radius: 68rem 68rem 0 0;
+    transform: rotate(90deg);
+    position: absolute;
+    top: 0.6rem;
+    left: -2rem;
+    transition: opacity .5s ease .3s;
+}
+
+.process-accordion .process-accordion__wrapper .process-activities li:before {
+  opacity: 0;
+}
+
+.process-accordion .process-accordion__wrapper .process-activities li.is-inview:before {
+  opacity: 1;
+}
+
+.process-accordion.active .process-accordion__wrapper .process-icon {
+  display: none;
+}
+
+.process-accordion .process-line {
+  height: 1px;
+  background: #dddfe2;
+}
+
+.process-accordion.active .process-line {
+  height: 3px;
+  background: #000;
+}
+</style>
---
+++ components/ProcessSection.vue
@@ -0,0 +1,211 @@
+<template>
+  <section id="process-section" class="process-section__wrapper">
+    <div class="section-container">
+      <div class="process-title">
+        <h2 class="process-header__text" data-splitting="lines" data-animation="text">
+          We&apos;ve created a <span class="highlight">process</span> that knits your product with the <span class="highlight">innovation</span> it deserves.
+        </h2>
+        <div class="process-btn__wrapper">
+          <a href="/" class="process-btn">
+            <span>
+              <span class="button-label">
+                <span class="inner">
+                  <span class="initial">
+                    What we do
+                  </span>
+                  <span class="animated">
+                    What we do
+                  </span>
+                </span>
+              </span>
+              <span class="button-icon">
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16.4" height="12.086">
+                    <g><g>
+                      <path fill="currentColor" d="M16.4 5.939v1.366l-5.194 4.781h-2.08l5.151-4.741H0V5.937Z" />
+                      <path fill="currentColor" d="M9.077 1.036H11.1l4.267 3.922-2.09-.001Z" />
+                    </g></g>
+                  </svg>
+                </div>
+              </span>
+            </span>
+          </a>
+        </div>
+      </div>
+      <ProcessAccordion
+        v-for="(item, index) in contents"
+        :key="index"
+        :index="index"
+        :active-id="activeId"
+        :title="item.title"
+        :image="require(`~/assets/images/${item.image}`)"
+        :activities="item.content"
+        @set-active="setActiveItem"
+      />
+      <button v-if="!expandAll" class="expand-all" @click="expandAllItems">
+        Expand All
+      </button>
+      <button v-else class="expand-all" data-scroll-offset="-100" @click="expandAllItems">
+        Expand Less
+      </button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      contents: [
+        {
+          title: 'Discovering the unforeseen',
+          image: 'unforeseen.webp',
+          content: ['UX Audit', 'Analytical data review', 'Market and User Research', 'Structuring true insights', 'User interviews and surveys', 'Information architecture (IA)']
+        },
+        {
+          title: 'Transforming the experience',
+          image: 'experience.jpeg',
+          content: ['User experience design (UX)', 'Usability testing', 'Product innovation and strategy', 'UX Writing', 'Wireframing and Rapid Prototyping']
+        },
+        {
+          title: 'Designing for impact',
+          image: 'airplane.jpeg',
+          content: ['User Interface Design (UI)', 'Augmented Reality (AR) explorations', 'Design strategy', '3D Design', 'Custom Illustrations & iconography', '2D Motion Design']
+        },
+        {
+          title: 'Scaling to be relevant',
+          image: 'style-guide.webp',
+          content: ['Front-end development', 'Innovation garages', 'Building design systems', 'Improving existing KPIs', 'Building style guides', 'Continuous product improvement']
+        }
+      ],
+      activeId: [0],
+      expandAll: false
+    }
+  },
+  methods: {
+    setActiveItem (id) {
+      if (this.activeId[0] !== id) {
+        this.activeId[0] = id
+      }
+    },
+    expandAllItems () {
+      this.expandAll = !this.expandAll
+      if (this.activeId.length < this.contents.length) {
+        this.contents.map((item, index) => {
+          this.activeId.splice(index, 1)
+          this.activeId.push(index)
+          return item
+        })
+      } else {
+        this.activeId = [-1]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.process-section__wrapper {
+  margin-bottom: 140px;
+}
+.process-section__wrapper .section-container {
+    padding: 0 12rem;
+}
+.process-title {
+    margin: 13.4rem 0 9.6rem;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+}
+.process-header__text {
+    max-width: 45rem;
+    margin-right: 4rem;
+    width: 100%;
+    font-size: 3rem;
+    line-height: 1.375;
+}
+.process-btn__wrapper {
+    flex-shrink: 0;
+}
+.process-btn {
+    position: relative;
+    cursor: pointer;
+    transition: background .2s,border .2s;
+    background: #000;
+    color: #fff;
+    border-radius: 5px;
+    padding: 1.4rem 1.6rem;
+    display: inline-flex;
+    align-items: center;
+}
+
+.process-btn:hover {
+    background: #dc7550;
+}
+
+.button-label {
+    display: inline-flex;
+    align-items: center;
+    position: relative;
+}
+
+.button-label .inner {
+    display: flex;
+    transition: transform .6s cubic-bezier(1,0,0,1);
+}
+
+.process-btn:hover .button-label .inner {
+    transition: transform .6s cubic-bezier(.19,1,.22,1);
+    transform: translateY(-125%);
+}
+
+.button-label .inner span {
+    display: flex;
+    white-space: nowrap;
+}
+
+.process-btn .button-label .inner .initial,
+.process-btn .button-label .inner .animated {
+    transition: opacity .6s cubic-bezier(1,0,0,1);
+}
+
+.process-btn:hover .button-label .inner .initial,
+.process-btn:hover .button-label .inner .animated {
+    transition: transform .6s cubic-bezier(.19,1,.22,1);
+}
+
+.process-btn:hover .button-label .inner .initial {
+    opacity: 0;
+}
+
+.process-btn:hover .button-label .inner .animated {
+    opacity: 1;
+}
+
+.button-label .inner .animated {
+    opacity: 0;
+    position: absolute;
+    top: 125%;
+}
+
+.button-icon {
+    margin-left: 12px;
+    transition: transform .3s cubic-bezier(.19,1,.22,1) 0s;
+    will-change: transform;
+    display: inline-block;
+    line-height: 0;
+}
+
+.expand-all {
+    display: block;
+    color: #dc7550;
+    cursor: pointer;
+    margin-left: auto;
+    border-bottom: 1px solid #dc7550;
+    max-width: 9.5rem;
+    margin-top: 3.4rem;
+    padding-bottom: 0.35rem;
+    font-size: 1.3rem;
+    font-family: "Telegraf";
+}
+</style>
